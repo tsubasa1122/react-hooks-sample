64017fb39da3b0e6f68908d6de817d187f4df5fe
tsubasa1122 <nakanot@open8.com>
2020-06-14 16:16:43 +0900

    implement callbacks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = props => {
   const [state, setState] = useState(props);
   const { name, price } = state;
+
+  useEffect(() => {
+    console.log("useEffect");
+  });
+
+  useEffect(() => {
+    console.log("componentDidmount");
+  }, []);
+
+  useEffect(() => {
+    console.log("name only callback");
+  }, [name]);
 
   return (
     <>
