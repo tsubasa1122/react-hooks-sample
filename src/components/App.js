2007595d9a80362846b6a2da4461a1a85eef64c1
tsubasa1122 <nakanot@open8.com>
2020-06-27 23:37:37 +0900

    refactor events

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,11 @@ import EventForm from './EventForm';
 import Events from './Events';
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, []);
+  const initialState = {
+    events: []
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <AppContext.Provider value={{ state, dispatch }} >
