5b2b6639d27667cb0c96ee93918f239df4d4592d
tsubasa1122 <nakanot@open8.com>
2020-07-05 23:37:26 +0900

    create delete_operation_logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -39,6 +39,14 @@ const EventForm = () => {
     };
   }
 
+  const deleteAllOperationLogs = e => {
+    e.preventDefault();
+    const result = window.confirm('全ての操作ログを本当に削除しても良いですか？')
+    if(result) {{
+      dispatch({type: DELETE_ALL_OPERATION_LOGS})
+    }}
+  }
+
   const unCreatable = title === '' || body === '';
 
   return (
@@ -56,6 +64,7 @@ const EventForm = () => {
 
         <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
         <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
+        <button className="btn btn-danger" onClick={deleteAllOperationLogs} disabled={state.operationLogs.length === 0}>全ての操作ログを削除する</button>
       </form>
     </>
   )
